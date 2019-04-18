96ef041e0c311669e58e34dae9b321e109a43b72
Yuichiro Ueno <y1r@rio.gsic.titech.ac.jp>
2019-04-18 14:28:35 +0900

    bugfix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <mpi.h>
 
+#include <sys/time.h>
 #include <getopt.h>
 
 #include <cassert>
 #include <iostream>
 #include <vector>
 
-constexpr int BLOCKSIZE = 16 * 1024 * 1024;
+constexpr long long BLOCKSIZE = 128 * 1024 * 1024;
+
+inline double get_time()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+}
 
 int main(int argc, char *argv[]) {
     int c;
@@ -39,9 +48,17 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
+    MPI_Info info;
+    MPI_Info_create(&info);
+
+    MPI_Info_set(info, "mpiio_coll_contiguous", "true");
+    MPI_Info_set(info, "coll_read_bufsize", "134217728");
+
     MPI_File fp;
     MPI_File_open(MPI_COMM_WORLD, filenames[0].c_str(), MPI_MODE_RDONLY,
-                  MPI_INFO_NULL, &fp);
+                  info, &fp);
+
+    MPI_File_set_atomicity(fp, 0);
 
     MPI_Offset filesize;
     MPI_File_get_size(fp, &filesize);
@@ -49,18 +66,27 @@ int main(int argc, char *argv[]) {
     std::cout << "rank: " << mpi_rank << ", filesize: " << filesize
               << std::endl;
 
-    char *buffer[BLOCKSIZE];
+    void *buffer = malloc(BLOCKSIZE);
 
-    int iterations = (filesize + (BLOCKSIZE - 1)) / BLOCKSIZE;
-    MPI_Status status;
+    long long iterations = (filesize + (BLOCKSIZE - 1)) / BLOCKSIZE;
 
     for (int i = 0; i < iterations; i++) {
-        MPI_File_read_all(fp, buffer, BLOCKSIZE, MPI_BYTE, &status);
-        int count;
-        MPI_Get_count(&status, MPI_BYTE, &count);
+        long long previous_step = std::max(0LL, i * BLOCKSIZE);
+        long long this_step = std::min(filesize, (i + 1) * BLOCKSIZE);
+
+        int count = this_step - previous_step;
+
+        double start = get_time();
+        MPI_File_read_all(fp, buffer, BLOCKSIZE, MPI_BYTE, MPI_STATUS_IGNORE);
+        double end = get_time();
+
+        double elapsed = end - start;
+
         std::cout << "rank: " << mpi_rank << ", received: " << count
-                  << std::endl;
+                  << "on: " << count / elapsed / 1000 / 1000 << "[MB/s]" << std::endl;
     }
+
+    free(buffer);
 
     MPI_File_close(&fp);
 
